0d3006715ac59f85e39e64fa081a69fd1dedb0ea
jplepinsk1 <jplepinsk.senai@gmail.com>
2025-02-19 09:27:11 -0300

    Primeiro Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+API = 'https://api.thecatapi.com/v1/images/search'
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    nome = request.form.get('name', None)
+    if not nome:
+        return render_template('index.html', erro="Você não informou o seu nome!")
+    
+    response = requests.get(API)
+
+    if response.status_code == 200:
+        data = response.json()
+        url = data[0]['url']
+        return render_template('index.html', nome=nome, url=url)
+    else:
+        return render_template('index.html', erro="Erro no sistema! Volte outra hora.")
+
+if __name__ == '__main__':
+    app.run(debug=True)
